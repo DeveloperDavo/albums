1649462f3845dae66922680690fe65f0b25f3e4b
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-03-23 08:03:42 +0100

    Display grid items on mount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import GridItem from './GridItem'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      albums: []
+    }
+  }
+
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/albums')
+    axios
+      .get('https://jsonplaceholder.typicode.com/albums')
+      .then(response => this.setState({ albums: response.data }))
   }
 
   render() {
-    return (
-      <div>
-      </div>
-    )
+    const gridItems = this.state.albums.map((album, index) => <GridItem key={index} />)
+    return <div>{gridItems}</div>
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,26 @@ import { shallow } from 'enzyme'
 import axios from 'axios'
 
 import App from './App'
+import GridItem from './GridItem'
 
 jest.mock('axios')
+
+const data = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'quidem molestiae enim'
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'sunt qui excepturi placeat culpa'
+  }
+]
+
+beforeEach(() => {
+  axios.get.mockResolvedValue({ data })
+})
 
 it('fetches albums on mount', () => {
   shallow(<App />)
@@ -12,4 +30,10 @@ it('fetches albums on mount', () => {
   expect(axios.get).toHaveBeenCalledWith(
     'https://jsonplaceholder.typicode.com/albums'
   )
+})
+
+it('displays grid items on mount', async () => {
+  const wrapper = await shallow(<App />)
+
+  expect(wrapper.find(GridItem)).toHaveLength(data.length)
 })
