10fd2252f1cd3c36df00e12c48a6d0da93346559
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-03-23 09:43:17 +0100

    Style grid and grid items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import GridItem from './GridItem'
+import './App.css'
 
 class App extends Component {
   constructor(props) {
@@ -20,7 +21,7 @@ class App extends Component {
     const gridItems = this.state.albums.map(album => (
       <GridItem key={album.id} title={album.title} userId={album.userId} />
     ))
-    return <div>{gridItems}</div>
+    return <div className="App">{gridItems}</div>
   }
 }
 
