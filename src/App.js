15e46ad984c33a2b069342c37b17854036e2f0a1
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-03-27 08:46:17 +0100

    Add route to single album

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import AlbumsContainer from './AlbumsContainer.js'
 import RedirectToAlbumStart from './RedirectToAlbumStart.js'
+import PhotosContainer from './PhotosContainer.js'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/albums" component={AlbumsContainer} />
+        <Route exact path="/albums/:albumId" component={PhotosContainer} />
         <Route component={RedirectToAlbumStart} />
       </Switch>
     </BrowserRouter>
---
+++ src/PhotosContainer.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function PhotosContainer() {
+  return <p>PhotosContainer</p>
+}
