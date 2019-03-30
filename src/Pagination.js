42307f5b56d44b4c9800c43c4275b840332967bf
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-03-30 17:59:08 +0100

    Add '/' to image sources in Pagination

--- src/Pagination.js
+++ src/Pagination.js
@@ -9,10 +9,10 @@ export default function Pagination(props) {
   return (
     <div className="Pagination">
       <button className={prevClassName} onClick={props.onPreviousClick}>
-        <img src="left-chevron.svg" alt="left chevron" />
+        <img src="/left-chevron.svg" alt="left chevron" />
       </button>
       <button className="Pagination__btn" onClick={props.onNextClick}>
-        <img src="right-chevron.svg" alt="right chevron" />
+        <img src="/right-chevron.svg" alt="right chevron" />
       </button>
     </div>
   )
