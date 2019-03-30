8dc7024a0f35caf8d60e4e4400a74574a10800a7
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-03-30 17:59:55 +0100

    Remove Component import from AlbumsContainer

--- src/AlbumsContainer.js
+++ src/AlbumsContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import axios from 'axios'
 import queryString from 'query-string'
 
@@ -12,7 +12,7 @@ import RedirectToAlbumStart from './RedirectToAlbumStart'
 import './AlbumsContainer.css'
 import './Error.css'
 
-class AlbumsContainer extends Component {
+class AlbumsContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
