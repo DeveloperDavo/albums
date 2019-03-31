bef99d3935e990021358e70d75aa95f647072988
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-03-31 21:14:03 +0200

    Fetch photos in placeholderClient

--- src/api/placeholderClient.js
+++ src/api/placeholderClient.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com/'
+
 export function fetchAlbums(start, limit) {
+  return axios.get(`${BASE_URL}albums?_start=${start}&_limit=${limit}`)
+}
+
+export function fetchPhotos(start, limit, albumId) {
   return axios.get(
-    `https://jsonplaceholder.typicode.com/albums?_start=${start}&_limit=${limit}`
+    `${BASE_URL}photos?albumId=${albumId}&_start=${start}&_limit=${limit}`
   )
 }
--- src/api/placeholderClient.test.js
+++ src/api/placeholderClient.test.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 import { fetchAlbums } from './placeholderClient'
+import { fetchPhotos } from './placeholderClient'
 
 jest.mock('axios')
 
@@ -26,5 +27,33 @@ describe('placeHolderClient', () => {
       'https://jsonplaceholder.typicode.com/albums?_start=20&_limit=30'
     )
     expect(albums).toEqual(data)
+  })
+
+  it('fetches photos', () => {
+    const data = [
+      {
+        albumId: 5,
+        id: 201,
+        title:
+          'nesciunt dolorum consequatur ullam tempore accusamus debitis sit',
+        url: 'https://via.placeholder.com/600/250289',
+        thumbnailUrl: 'https://via.placeholder.com/150/250289'
+      },
+      {
+        albumId: 5,
+        id: 202,
+        title: 'explicabo vel omnis corporis debitis qui qui',
+        url: 'https://via.placeholder.com/600/6a0f83',
+        thumbnailUrl: 'https://via.placeholder.com/150/6a0f83'
+      }
+    ]
+
+    axios.get.mockReturnValue(data)
+    const photos = fetchPhotos(20, 30, 5)
+
+    expect(axios.get).toHaveBeenCalledWith(
+      'https://jsonplaceholder.typicode.com/photos?albumId=5&_start=20&_limit=30'
+    )
+    expect(photos).toEqual(data)
   })
 })
