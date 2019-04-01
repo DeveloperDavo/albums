e0be36517e61cc38f6339281edef879e1f405a51
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-04-01 22:37:49 +0200

    Add minimal modal

--- src/PhotoGridItem.js
+++ src/PhotoGridItem.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactModal from 'react-modal'
 
 import './GridItem.css'
 
-export default function PhotoGridItem(props) {
-  const { title, thumbnailUrl } = props
-  return (
-    <div className="GridItem">
-      <img src={thumbnailUrl} alt={title} />
-      <p className="GridItem__title">{title}</p>
-    </div>
-  )
+ReactModal.setAppElement('#root')
+
+export default class PhotoGridItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false
+    }
+  }
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true })
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false })
+  }
+
+  render() {
+    const { title, thumbnailUrl } = this.props
+    return (
+      <div className="GridItem">
+        <img onClick={this.handleOpenModal} src={thumbnailUrl} alt={title} />
+        <p className="GridItem__title">{title}</p>
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="Minimal Modal Example"
+        >
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
+      </div>
+    )
+  }
 }
 
 PhotoGridItem.propTypes = {
