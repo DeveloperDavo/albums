a83b37066a0a03eaa3ce11ad23188ce29a60e793
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-04-01 22:39:36 +0200

    Add custom styles to modal

--- src/PhotoGridItem.js
+++ src/PhotoGridItem.js
@@ -4,6 +4,17 @@ import ReactModal from 'react-modal'
 
 import './GridItem.css'
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
+
 ReactModal.setAppElement('#root')
 
 export default class PhotoGridItem extends React.Component {
@@ -30,7 +41,8 @@ export default class PhotoGridItem extends React.Component {
         <p className="GridItem__title">{title}</p>
         <ReactModal
           isOpen={this.state.showModal}
-          contentLabel="Minimal Modal Example"
+          contentLabel="title of photo"
+          style={customStyles}
         >
           <button onClick={this.handleCloseModal}>Close Modal</button>
         </ReactModal>
