c80d924b082c749d573c5fcca1e3946f66769661
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-04-02 08:26:26 +0200

    Lift Modal up to Photos

--- src/PhotoGridItem.js
+++ src/PhotoGridItem.js
@@ -1,56 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ReactModal from 'react-modal'
 
 import './GridItem.css'
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
+export default function PhotoGridItem(props) {
+  const { title, thumbnailUrl } = props
+  return (
+    <div onClick={props.onClick} className="GridItem">
+      <img src={thumbnailUrl} alt={title} />
+      <p className="GridItem__title">{title}</p>
+    </div>
+  )
 }
 
-ReactModal.setAppElement('#root')
-
-export default class PhotoGridItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showModal: false
-    }
-  }
-
-  handleOpenModal = () => {
-    this.setState({ showModal: true })
-  }
-
-  handleCloseModal = () => {
-    this.setState({ showModal: false })
-  }
-
-  render() {
-    const { title, thumbnailUrl } = this.props
-    return (
-      <div className="GridItem">
-        <img onClick={this.handleOpenModal} src={thumbnailUrl} alt={title} />
-        <p className="GridItem__title">{title}</p>
-        <ReactModal
-          isOpen={this.state.showModal}
-          contentLabel="title of photo"
-          style={customStyles}
-        >
-          <button onClick={this.handleCloseModal}>Close Modal</button>
-        </ReactModal>
-      </div>
-    )
-  }
-}
-
+// TODO
 PhotoGridItem.propTypes = {
   title: PropTypes.string.isRequired,
   thumbnailUrl: PropTypes.string.isRequired
--- src/Photos.js
+++ src/Photos.js
@@ -1,25 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactLoading from 'react-loading'
+import ReactModal from 'react-modal'
 
 import PhotoGridItem from './PhotoGridItem'
 
-export default function Photos(props) {
-  const { loading, photos } = props
+ReactModal.setAppElement('#root')
 
-  const gridItems = photos.map(photo => (
-    <PhotoGridItem
-      key={photo.id}
-      title={photo.title}
-      thumbnailUrl={photo.thumbnailUrl}
-    />
-  ))
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
 
-  return loading ? (
-    <ReactLoading className="loading" type={'spokes'} color={'black'} />
-  ) : (
-    <div className="Grid">{gridItems}</div>
-  )
+export default class Photos extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false
+    }
+  }
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true })
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false })
+  }
+
+  render() {
+    const { loading, photos } = this.props
+
+    const gridItems = photos.map(photo => (
+      <PhotoGridItem
+        key={photo.id}
+        title={photo.title}
+        thumbnailUrl={photo.thumbnailUrl}
+        onClick={this.handleOpenModal}
+      />
+    ))
+
+    return loading ? (
+      <ReactLoading className="loading" type={'spokes'} color={'black'} />
+    ) : (
+      <>
+        <div className="Grid">{gridItems}</div>
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="title of photo"
+          style={customStyles}
+        >
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
+      </>
+    )
+  }
 }
 
 Photos.propTypes = {
