a5a802c7e03989adc551ec41c1df87d349738d6f
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-06-12 09:28:04 +0200

    Replace withItemFetcher in ContainerWithAlbums

--- src/component/album/ContainerWithAlbums.js
+++ src/component/album/ContainerWithAlbums.js
@@ -1,35 +1,69 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import queryString from 'query-string'
 
 import Container from '../Container'
 import Albums from './Albums'
-import withItemFetcher from '../withItemFetcher'
 import { fetchAlbums } from '../../api/placeHolderClient'
 
 import '../Error.css'
 
-export function ContainerWithAlbums(props) {
-  const { history, items, loading, location } = props
+export default class ContainerWithAlbums extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      empty: false,
+      error: false,
+      items: [],
+      loading: false
+    }
+  }
 
-  return (
-    <Container {...props}>
-      <Albums
-        albums={items}
-        history={history}
-        loading={loading}
-        location={location}
-      />
-    </Container>
-  )
+  getItems = () => {
+    const { location } = this.props
+    const { start, limit } = queryString.parse(location.search)
+    if (!isNaN(start) && !isNaN(limit)) {
+      this.setState({ loading: true, empty: false, error: false, items: [] })
+      fetchAlbums(this.props)
+        .then(response => {
+          if (response.data.length === 0) this.setState({ empty: true })
+          this.setState({
+            items: response.data,
+            loading: false
+          })
+        })
+        .catch(error => this.setState({ error: true, loading: false }))
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.location.search !== prevProps.location.search) {
+      this.getItems()
+    }
+  }
+
+  componentDidMount() {
+    this.getItems()
+  }
+
+  render() {
+    const { items, loading } = this.state
+    const { history, location } = this.props
+
+    return (
+      <Container {...this.props}>
+        <Albums
+          albums={items}
+          history={history}
+          loading={loading}
+          location={location}
+        />
+      </Container>
+    )
+  }
 }
 
-export default withItemFetcher(ContainerWithAlbums, fetchAlbums)
-
 ContainerWithAlbums.propTypes = {
-  empty: PropTypes.bool.isRequired,
-  error: PropTypes.bool.isRequired,
-  items: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
   location: PropTypes.shape({
     search: PropTypes.string.isRequired
   }).isRequired
